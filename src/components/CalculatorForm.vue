7ee023b9c5d9cc56971b7d7d4ba66ea81537aabe
wenchonghuang <huangkingsley@gmail.com>
2019-09-07 01:07:56 +1200

    home value input done

--- src/components/CalculatorForm.vue
+++ src/components/CalculatorForm.vue
@@ -6,18 +6,17 @@
           <input
             type="tel"
             maxlength="3"
-            class="form-control js-calculator-reverse-mortgage-widget__ageFirst"
+            class="form-control"
             v-model="formData.age.value"
             @keypress="onTouch($event,formData.age)"
-            @blur="ageFocus(formData.age)"
+            @blur="ageBlur(formData.age)"
             autocomplete="off"
           />
-          <label
-            id="js-youngest-borrower-age"
-            class="input-label"
-            v-bind:class="{ active: formData.age.touched}"
-          >Enter your age</label>
-          <span>{{this.formData.age.errorMessage}}</span>
+          <label class="input-label" v-bind:class="{ active: formData.age.touched}">Enter your age</label>
+          <span
+            id="heroCalculatorAge_error"
+            v-bind:class="{ 'validation-warning': formData.age.hasError}"
+          >{{formData.age.errorMessage}}</span>
         </div>
       </div>
     </div>
@@ -26,15 +25,21 @@
         <div class="position-relative text-left mt-4">
           <input
             type="tel"
-            class="form-control calculator-reverse-mortgage-widget__houseValue js-calculator-reverse-mortgage-widget__houseValue"
-            id="heroCalculatorHouseValue"
-            name="houseValue"
-            onkeypress="return event.charCode >= 48 && event.charCode <= 57"
+            class="form-control"
+            v-model="homeDisplay"
+            @keypress="onTouch($event,formData.homeValue)"
+            @blur="homeBlur(formData.homeValue)"
             required
             autocomplete="off"
           />
-          <label class="input-label">Enter your home value</label>
-          <span id="heroCalculatorHouseValue_error">Minimum property value is $200,000</span>
+          <label
+            class="input-label"
+            v-bind:class="{ active: formData.homeValue.touched}"
+          >Enter your home value</label>
+          <span
+            id="heroCalculatorHouseValue_error"
+            v-bind:class="{ 'validation-warning': formData.homeValue.hasError}"
+          >Minimum property value is $200,000</span>
         </div>
       </div>
     </div>
@@ -75,8 +80,14 @@ export default {
         age: {
           value: undefined,
           touched: false,
-          hasError:false,
-          errorMessage:"Minimum age is 60 years",
+          hasError: false,
+          errorMessage: "Minimum age is 60 years"
+        },
+        homeValue: {
+          value: undefined,
+          touched: false,
+          hasError: false,
+          errorMessage: "Minimum property value is $200,000"
         }
       }
     };
@@ -84,21 +95,52 @@ export default {
   computed: {
     ageValue() {
       return this.formData.age.value;
+    },
+    homeValue() {
+      return this.formData.homeValue.value;
+    },
+    homeDisplay: {
+      get() {
+        if (this.formData.homeValue.value === "$") {
+          return this.formData.homeValue.value;
+        }
+        if (this.formData.homeValue.value) {
+          var stripped = this.formData.homeValue.value.replace(/[$,]/g, "");
+          console.log(stripped);
+          if (stripped) {
+            const formattedStripped = parseInt(stripped).toLocaleString("en", {
+              currency: "USD",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 7
+            });
+            return "$" + formattedStripped;
+          }
+        }
+        return "";
+      },
+      set(newValue) {
+        this.formData.homeValue.value = newValue;
+      }
     }
   },
   watch: {
     ageValue(value) {
-    //    const intValue=parseInt(value);
-    //    console.log(intValue, intValue<60);
-    //     this.formData.age.errorMessage="Minimum age is 60 years";
-    //     this.formData.age.hasError=false;
-    //     if(intValue<60||intValue>120){
-    //         this.formData.age.hasError=true;
-    //     }
-    //     if(intValue>120){
-    //         this.formData.age.errorMessage="Are you intending to beat the world record? :P";
-    //     }
-
+      this.formData.age.errorMessage = "Minimum age is 60 years";
+      this.formData.age.hasError = false;
+      if (value < 60 || value > 120) {
+        this.formData.age.hasError = true;
+      }
+      if (value > 120) {
+        this.formData.age.errorMessage =
+          "Are you intending to beat the world record? :P";
+      }
+    },
+    homeValue(value) {
+      var stripped = value.replace(/[$,]/g, "");
+      this.formData.homeValue.hasError = false;
+      if (stripped < 200000) {
+        this.formData.homeValue.hasError = true;
+      }
     }
   },
 
@@ -110,10 +152,16 @@ export default {
       }
       e.preventDefault();
     },
-    ageFocus(input) {
+    ageBlur(input) {
       if (!input.value) {
         input.value = 0;
         input.touched = true;
+      }
+    },
+    homeBlur(input) {
+      input.touched = true;
+      if (input.value == 0) {
+        input.value = "$";
       }
     }
   }
